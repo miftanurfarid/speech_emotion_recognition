29e54ddd0a403dac7508288fbb833819e453f84c
Mifta Nur Farid <miftanurfarid@gmail.com>
2021-11-21 15:38:34 +0800

    mlp for speech

---
+++ code/mlp_sd_speech.py
@@ -0,0 +1,111 @@
+# mlp_sd.py: emotion recognition using multilayer perceptron with speaker dependent
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random as rn
+
+np.random.seed(123)
+rn.seed(123)
+tf.random.set_seed(123)
+
+# load feature data
+data_path = '../data/speech/' # choose song or speech
+X = np.load(data_path + 'x.npy')
+y = np.load(data_path + 'y.npy')
+
+# split into train and test
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                             patience=10,
+                                             restore_best_weights=True)
+
+checkpointer = tf.keras.callbacks.ModelCheckpoint(
+    filepath='/tmp/weights.hdf5', verbose=1, save_best_only=True)
+
+# function to build model
+def model_dense():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.BatchNormalization(axis=-1,
+              input_shape=(x_train.shape[1:])))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.4))
+    model.add(tf.keras.layers.Dense(6, activation='softmax'))
+
+    # compile model: set loss, optimizer, metric
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=['accuracy'])
+    return model
+
+# create the model
+model = model_dense()
+print(model.summary())
+
+# plot model
+tf.keras.utils.plot_model(model,'mlp_model_sd_speech.pdf',show_shapes=True)
+
+# train the model
+hist = model.fit(x_train, 
+                 y_train, 
+                 epochs=100, 
+                 shuffle=True,
+                 #callbacks=earlystop,
+                 validation_split=0.1,
+                 batch_size=16)
+
+# evaluate the model on test partition
+evaluate = model.evaluate(x_test, y_test, batch_size=16)
+print("Loss={:.6f}, Accuracy={:.6f}".format(evaluate[0],evaluate[1]))
+
+# plot accuracy
+plt.figure()
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.grid()
+plt.legend(['Training', 'Validation'])
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('MLP Model')
+plt.savefig('mlp_accuracy_sd_speech.svg')
+
+# make prediction for confusion_matrix
+import os
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import pandas as pd
+predict = model.predict(x_test, batch_size=16)
+emotions = ['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprise']
+
+# predicted emotions from the test set
+y_pred = np.argmax(predict, 1)
+predicted_emo = []
+for i in range(0,y_test.shape[0]):
+    emo = emotions[y_pred[i]]
+    predicted_emo.append(emo)
+
+# get actual emotion
+actual_emo = []
+y_true = y_test
+for i in range(0,y_test.shape[0]):
+    emo = emotions[y_true[i]]
+    actual_emo.append(emo)
+
+# generate the confusion matrix
+cm = confusion_matrix(actual_emo, predicted_emo)
+cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+index = ['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprise']
+columns = ['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprise']
+cm_df = pd.DataFrame(cm, index, columns)
+plt.figure(figsize=(10, 6))
+plt.title('Confusion Matrix of MLP')
+sns.heatmap(cm_df, annot=True)
+plt.savefig('mlp_cm_sd_speech.svg')
+
+# print unweighted average recall
+print("UAR: ", cm.trace()/cm.shape[0])
